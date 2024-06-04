2b458d08aa0050dd3d710e2a6826761c57e1f9a8
Vinicius040496 <vinicius.almeida2011@gmail.com>
2024-06-04 10:58:21 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp126/Funcionario.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+namespace Course
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem / 100.0);
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", R$ "
+            + SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConsoleApp126/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salario bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine());
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Funcionário: " + func);
+            Console.WriteLine();
+            Console.Write("Deseja aumentar o salário em qual porcentagem? ");
+            double porcent = double.Parse(Console.ReadLine());
+            func.AumentarSalario(porcent);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados: " + func);
+            Console.ReadLine();
+        }
+    }
+}
